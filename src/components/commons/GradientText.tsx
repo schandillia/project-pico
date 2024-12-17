8ef5ceb6520095c6d1c166028f8dd27b031e89fa
schandillia <amit@schandillia.com>
2024-12-17 19:12:28 +0530

    CHORE: Add gradient text and gradient effect on landing page bg

---
+++ src/components/commons/GradientText.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from "react"
+import { cn } from "@/lib/utils"
+
+const tags: string[] = ["h1", "h2", "h3", "h4", "h5", "h6"]
+
+interface GradientTextProps extends React.HTMLAttributes<HTMLElement> {
+	children: ReactNode // Specify ReactNode type for children prop
+	className?: string
+	type?: string
+}
+
+export default function GradientText({
+	children,
+	className = "",
+	type = "p",
+}: GradientTextProps) {
+	const style = `leading-tight ${cn(
+		"bg-gradient-to-r from-blue-400 via-blue-800 to-blue-500 text-transparent bg-clip-text",
+		className,
+	)}`
+	if (tags.includes(type))
+		return React.createElement(type, { className: style }, children)
+	return React.createElement("p", { className: style }, children)
+}
