b2cd294858c84375bf86f0bdd7160fcbef1d26da
schandillia <amit@schandillia.com>
2024-12-20 16:35:59 +0530

    CHORE: Remove content DRY.

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -1,30 +1,51 @@
-import ContentPage from "@/components/commons/ContentPage"
+import MaxWidthWrapper from "@/components/commons/MaxWidthWrapper"
 import brand from "@/constants/brand.json"
-import { Metadata } from "next"
+import fs from "fs/promises"
+import path from "path"
+import { marked } from "marked"
+import type { Metadata } from "next"
 
 export const metadata: Metadata = {
 	title: `Privacy Policy | ${brand.BRAND}`,
 	description: `Read ${brand.BRAND}’s Privacy Policy to learn about the information we collect, how it’s used, and the control you have over it.`,
 }
 
-export default function Page() {
+async function getTermsContent() {
+	const filePath = path.join(process.cwd(), "src", "documents", "PRIVACY.md")
+	const content = await fs.readFile(filePath, "utf8")
+
+	// Get the current date and format it to "January 13, 2024"
+	const currentDate = new Date()
+	const formattedDate = currentDate.toLocaleDateString("en-US", {
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+	})
+
+	const processedContent = content
+		.replace(/{date}/g, formattedDate)
+		.replace(/{brand}/g, brand.BRAND)
+		.replace(/{company}/g, brand.COMPANY)
+		.replace(/{site}/g, brand.SITE)
+		.replace(/{email}/g, brand.SUPPORT_EMAIL)
+		.replace(/{twitter_handle}/g, brand.TWITTER_HANDLE)
+
+	return marked(processedContent)
+}
+
+export default async function Page() {
+	const htmlContent = await getTermsContent()
+
 	return (
-		<ContentPage
-			fileName="PRIVACY.md"
-			title={`Privacy Policy | ${brand.BRAND}`}
-			description={`Read ${brand.BRAND}’s Privacy Policy to learn about the information we collect, how it’s used, and the control you have over it.`}
-			replaceValues={{
-				date: new Date().toLocaleDateString("en-US", {
-					year: "numeric",
-					month: "long",
-					day: "numeric",
-				}),
-				brand: brand.BRAND,
-				company: brand.COMPANY,
-				site: brand.SITE,
-				email: brand.SUPPORT_EMAIL,
-				twitter_handle: brand.TWITTER_HANDLE,
-			}}
-		/>
+		<MaxWidthWrapper className="flex flex-col">
+			<div className="w-full mx-auto py-8 px-4 sm:px-8 lg:px-10">
+				<div
+					className="prose lg:prose-lg prose-a:text-primary prose-h4:text-zinc-500 dark:prose-h4:text-zinc-400 dark:prose-invert max-w-full mx-auto bg-zinc-200 dark:bg-zinc-700 p-10 rounded-lg"
+					dangerouslySetInnerHTML={{
+						__html: htmlContent,
+					}}
+				/>
+			</div>
+		</MaxWidthWrapper>
 	)
 }
--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -1,29 +1,50 @@
-import ContentPage from "@/components/commons/ContentPage"
+import MaxWidthWrapper from "@/components/commons/MaxWidthWrapper"
 import brand from "@/constants/brand.json"
-import { Metadata } from "next"
+import fs from "fs/promises"
+import path from "path"
+import { marked } from "marked"
+import type { Metadata } from "next"
 
 export const metadata: Metadata = {
 	title: `Terms of Service | ${brand.BRAND}`,
 	description: `Read ${brand.BRAND}’s Terms of Service to understand the rules governing your access of all ${brand.BRAND} services.`,
 }
 
-export default function Page() {
+async function getTermsContent() {
+	const filePath = path.join(process.cwd(), "src", "documents", "TERMS.md")
+	const content = await fs.readFile(filePath, "utf8")
+
+	// Get the current date and format it to "January 13, 2024"
+	const currentDate = new Date()
+	const formattedDate = currentDate.toLocaleDateString("en-US", {
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+	})
+
+	const processedContent = content
+		.replace(/{date}/g, formattedDate)
+		.replace(/{brand}/g, brand.BRAND)
+		.replace(/{company}/g, brand.COMPANY)
+		.replace(/{email}/g, brand.SUPPORT_EMAIL)
+		.replace(/{twitter_handle}/g, brand.TWITTER_HANDLE)
+
+	return marked(processedContent)
+}
+
+export default async function Page() {
+	const htmlContent = await getTermsContent()
+
 	return (
-		<ContentPage
-			fileName="TERMS.md"
-			title={`Terms of Service | ${brand.BRAND}`}
-			description={`Read ${brand.BRAND}’s Terms of Service to understand the rules governing your access of all ${brand.BRAND} services.`}
-			replaceValues={{
-				date: new Date().toLocaleDateString("en-US", {
-					year: "numeric",
-					month: "long",
-					day: "numeric",
-				}),
-				brand: brand.BRAND,
-				company: brand.COMPANY,
-				email: brand.SUPPORT_EMAIL,
-				twitter_handle: brand.TWITTER_HANDLE,
-			}}
-		/>
+		<MaxWidthWrapper className="flex flex-col">
+			<div className="w-full mx-auto py-8 px-4 sm:px-8 lg:px-10">
+				<div
+					className="prose lg:prose-lg prose-a:text-primary prose-h4:text-zinc-500 dark:prose-h4:text-zinc-400 dark:prose-invert max-w-full mx-auto bg-zinc-200 dark:bg-zinc-700 p-10 rounded-lg"
+					dangerouslySetInnerHTML={{
+						__html: htmlContent,
+					}}
+				/>
+			</div>
+		</MaxWidthWrapper>
 	)
 }
