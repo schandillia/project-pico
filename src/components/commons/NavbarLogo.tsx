212569bb81815dd8402698b31516d479ec46c691
schandillia <amit@schandillia.com>
2024-12-21 14:35:27 +0530

    CHORE: Remove brand assets to public folder.

--- src/components/commons/NavbarLogo.tsx
+++ src/components/commons/NavbarLogo.tsx
@@ -1,15 +1,16 @@
 "use client"
 
 import Image from "next/image"
-import logo from "@/app/brand/logo.svg"
 import brand from "@/constants/brand.json"
 
 export default function NavbarLogo() {
 	return (
 		<Image
-			src={logo}
+			src="/brand/logo.svg"
 			alt={brand.BRAND}
 			className="size-8"
+			width={50}
+			height={50}
 			style={{
 				filter: "invert(45%) sepia(40%) saturate(400%) hue-rotate(122deg) brightness(95%) contrast(95%)",
 			}}
