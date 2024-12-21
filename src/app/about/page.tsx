e71d6957d16d9562aa153373cc739c110525267f
schandillia <amit@schandillia.com>
2024-12-21 20:34:34 +0530

    STYLE: Add viewport wrapper to about page.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from "next"
 import brand from "@/constants/brand.json"
+import ViewportWrapper from "@/components/commons/ViewportWrapper"
 
 export const metadata: Metadata = {
 	title: `About | ${brand.BRAND}`,
 	description: `Read ${brand.BRAND}’s Privacy Policy to learn about the information we collect, how it’s used, and the control you have over it.`,
 }
 export default function Page() {
-	return <div>about</div>
+	return (
+		<ViewportWrapper className="flex flex-col">
+			<div>about</div>
+		</ViewportWrapper>
+	)
 }
