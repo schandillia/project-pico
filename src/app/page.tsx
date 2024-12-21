1d57e43c2126a9b8544b62d1ec56b8de676e226d
schandillia <amit@schandillia.com>
2024-12-21 13:48:29 +0530

    CHORE: Minor change to landing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
 
 	return (
 		<>
-			{/* Hero Section */}
+			{/* Top Section */}
 			<ViewportWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
 				<h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl text-soft dark:text-white">
 					Create <span className="text-primary">threads</span> in a
