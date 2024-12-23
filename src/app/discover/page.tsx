304d383f1c32ab64a60f07c79bb219e1966d9dcc
schandillia <amit@schandillia.com>
2024-12-23 19:19:00 +0530

    CHORE: Scaffold for Discover.

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -1,8 +1,18 @@
-export default function page() {
+import type { Metadata } from "next"
+
+import PageHeading from "@/components/commons/PageHeading"
+import ViewportWrapper from "@/components/commons/ViewportWrapper"
+
+import brand from "@/lib/constants/brand.json"
+
+export const metadata: Metadata = {
+	title: `About | ${brand.BRAND}`,
+	description: `Read ${brand.BRAND}’s Privacy Policy to learn about the information we collect, how it’s used, and the control you have over it.`,
+}
+export default function Page() {
 	return (
-		<div>
-			<h1>Discover</h1>
-			<p>Welcome to the About page of our application.</p>
-		</div>
+		<ViewportWrapper className="my-8 flex flex-col">
+			<PageHeading>Discover</PageHeading>
+		</ViewportWrapper>
 	)
 }
