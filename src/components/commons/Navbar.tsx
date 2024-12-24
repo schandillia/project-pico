ee9542279a2ae9cb1a4ab16cbac9de0f951b1f8e
schandillia <amit@schandillia.com>
2024-12-24 12:48:56 +0530

    FEAT: Add navigational sidebar.

--- src/components/commons/Navbar.tsx
+++ src/components/commons/Navbar.tsx
@@ -1,11 +1,22 @@
 import Link from "next/link"
 
+import { Menu } from "lucide-react"
+
 import NavbarLogo from "@/components/commons/NavbarLogo"
 import SignIn from "@/components/commons/SignIn"
 import UserAccountNav from "@/components/commons/UserAccountNav"
 import ViewportWrapper from "@/components/commons/ViewportWrapper"
 import ThemeToggle from "@/components/theme/theme-toggle"
-import { buttonVariants } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
+import {
+	Sheet,
+	SheetClose,
+	SheetContent,
+	SheetFooter,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+} from "@/components/ui/sheet"
 
 import brand from "@/lib/constants/brand.json"
 
@@ -43,15 +54,50 @@ const Navbar = ({ session }: { session: Session | null }) => {
 			<ViewportWrapper>
 				<div className="flex h-14 items-center justify-between border-b border-gray-200 dark:border-gray-800">
 					{/* Brand Logo */}
-					<Link
-						href="/"
-						className="flex z-40 font-semibold gap-2 items-center"
-					>
-						<NavbarLogo />
-						<span className="text-lg text-soft dark:text-white/70">
-							{brand.BRAND}
-						</span>
-					</Link>
+					<div className="flex items-center">
+						<Sheet>
+							<SheetTrigger asChild>
+								{/* Add md:hidden to hide <Menu> icon on desktop */}
+								<Button
+									variant="outline"
+									size="icon"
+									className="mr-2"
+								>
+									<Menu />
+									<span className="sr-only">Toggle menu</span>
+								</Button>
+							</SheetTrigger>
+							<SheetContent side="left">
+								<SheetHeader>
+									<SheetTitle>{brand.BRAND}</SheetTitle>
+								</SheetHeader>
+								<div className="grid gap-4 py-4">
+									<div className="grid grid-cols-4 items-center gap-4">
+										<p>test</p>
+									</div>
+									<div className="grid grid-cols-4 items-center gap-4">
+										<p>test 2</p>
+									</div>
+								</div>
+								<SheetFooter>
+									<SheetClose asChild>
+										<Button type="submit">
+											Save changes
+										</Button>
+									</SheetClose>
+								</SheetFooter>
+							</SheetContent>
+						</Sheet>
+						<Link
+							href="/"
+							className="flex z-40 font-semibold gap-2 items-center"
+						>
+							<NavbarLogo />
+							<span className="text-lg text-soft dark:text-white/70">
+								{brand.BRAND}
+							</span>
+						</Link>
+					</div>
 
 					{/* TODO: Add mobile navbar */}
 
